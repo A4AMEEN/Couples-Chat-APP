ddffcdc173d833dc8b557cbea451dad038c55964
Ameen-Synram <ameen@synram.co>
2025-04-24 14:07:10 +0530

    new committs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,3 +4,9 @@ export const environment = {
   socketUrl: "/",
   vapidPublicKey: "BF7URUAWs0ApVJ1_DIuZXdW252657qmlUmkMLn0uCcZtoXhic9sJi5_pI9qqRONLmWNM0RJHIF4ESWLh5ilxA9A",
 }
+// export const environment = {
+//   production: true,
+//   apiUrl: "/api",
+//   socketUrl: "/",
+//   vapidPublicKey: "BF7URUAWs0ApVJ1_DIuZXdW252657qmlUmkMLn0uCcZtoXhic9sJi5_pI9qqRONLmWNM0RJHIF4ESWLh5ilxA9A",
+// }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,12 @@
 export const environment = {
   production: false,
-  apiUrl: "http://localhost:3000/api",
-  socketUrl: "http://localhost:3000",
+  apiUrl: "https://belleserver.vercel.app/api",
+  socketUrl: "https://belleserver.vercel.app/",
   vapidPublicKey: "BF7URUAWs0ApVJ1_DIuZXdW252657qmlUmkMLn0uCcZtoXhic9sJi5_pI9qqRONLmWNM0RJHIF4ESWLh5ilxA9A",
 }
+// export const environment = {
+//   production: false,
+//   apiUrl: "http://localhost:3000/api",
+//   socketUrl: "http://localhost:3000",
+//   vapidPublicKey: "BF7URUAWs0ApVJ1_DIuZXdW252657qmlUmkMLn0uCcZtoXhic9sJi5_pI9qqRONLmWNM0RJHIF4ESWLh5ilxA9A",
+// }
